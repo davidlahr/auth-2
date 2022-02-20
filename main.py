3b6220d1462763dc0528d77afa6602beb835ca1c
davidlahr <85328443+davidlahr@users.noreply.github.com>
2022-02-20 09:53:42 -0500

    updates to database url

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///users_auth2.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
 
