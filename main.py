3284f9b0fed6ec581d38e4c1255b47569b748065
davidlahr <85328443+davidlahr@users.noreply.github.com>
2022-02-19 19:11:07 -0500

    favicon

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ new_account = User(
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/')
+@app.route('/favicon.ico')
 def index():
     return render_template("index.html")
 
